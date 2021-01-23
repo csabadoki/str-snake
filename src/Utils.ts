1d98985df0c6246bfc04d653478e91b542a324fe
Bartha Csaba <csabartha@gmail.com>
2021-01-23 12:54:38 +0100

    Utils, Piece, Level

--- src/Utils.ts
+++ src/Utils.ts
@@ -11,7 +11,12 @@ class Debouncer {
      * @returns {number} - válaszd ki a kisebbet a num és a max közül 
      * majd válaszd ki a nagyobbat az előbbi érték és a min közül.
      */
-    
+    static bound (num: number, min: number, max: number) {
+        let nr: number;
+        nr = num < max ? num : max;
+        nr = num > min ? num : min;
+        return nr;
+    }
 
     static debounce<T extends Function>(fun: T, wait: number) {
         let id: any;
@@ -43,7 +48,9 @@ class Utils extends Debouncer {
      * @param el {Element} - egy DOM Element típus
      * @returns {void}
      */
-
+    static removeNode(el: Element) {
+        el.parentNode.removeChild(el);
+    }
 
 }
 
